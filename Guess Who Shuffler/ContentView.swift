682183628911d8722d30a4668d8b34d41ed743f6
Bryan Hunt <bryan@bryanhunt.dev>
2026-03-01 13:19:40 -0500

    Initial Commit

---
+++ Guess Who Shuffler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess Who Shuffler
+//
+//  Created by Bryan Hunt on 3/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guess Who Shuffler/Guess_Who_ShufflerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_Who_ShufflerApp.swift
+//  Guess Who Shuffler
+//
+//  Created by Bryan Hunt on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Guess_Who_ShufflerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
